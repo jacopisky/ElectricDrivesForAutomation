ac674e8fbd35519d5ccd3ffe297c697cb156ece6
Jacopo Pellizzari <jacopisky@gmail.com>
2021-12-10 15:09:54 +0100

    Modify

--- arduino/prova_motore/prova_motore.ino
+++ arduino/prova_motore/prova_motore.ino
@@ -6,8 +6,9 @@
 #define B  10
 #define Bp 9
 
-#define Tstep 2000    // us
+#define Tstep 3000    // us
 #define DELTA_HALF_STEP 0.9
+#define DELTA_FULL_STEP 1.8
 
 #define FORWARD   1
 #define IDLE_ANG  0
@@ -24,10 +25,17 @@ const uint8_t half_step[8][4] = {
   {1,0,0,1},
 };
 
+const uint8_t full_step[4][4] = {
+  {1,0,0,0},
+  {0,0,1,0},
+  {0,1,0,0},
+  {0,0,0,1},
+};
+
 uint8_t state[4] = {0,0,0,0};
 uint8_t i = 0;
 int counter = 0;
-float target = 0; // set here the angle in degrees
+float target = 90; // set here the angle in degrees
 
 Timer<1, micros> timer;
 
@@ -59,6 +67,10 @@ float getStepperAngle(){
   return DELTA_HALF_STEP * counter;
 }
 
+float getFullStepperAngle(){
+  return DELTA_FULL_STEP * counter;
+}
+
 void stepper(uint8_t dir){
   uint8_t prev = i;
   if(dir == FORWARD){
@@ -84,6 +96,35 @@ void stepper(uint8_t dir){
   }
   if(half_step[i][3] != half_step[prev][3]){
     digitalWrite(Bp, half_step[i][3]);
+  }
+  counter += dir;
+}
+
+void full_stepper(uint8_t dir){
+  uint8_t prev = i;
+  if(dir == FORWARD){
+    if(i == 3){
+      i = -1;
+    }
+    i++;
+  }
+  else if(dir == BACKWARD){
+    if(i == 0){
+      i = 4;
+    }
+    i--;
+  }
+  if(full_step[i][0] != full_step[prev][0]){
+    digitalWrite(A, full_step[i][0]);
+  }
+  if(full_step[i][1] != full_step[prev][1]){
+    digitalWrite(Ap, full_step[i][1]);
+  }
+  if(full_step[i][2] != full_step[prev][2]){
+    digitalWrite(B, full_step[i][2]);
+  }
+  if(full_step[i][3] != full_step[prev][3]){
+    digitalWrite(Bp, full_step[i][3]);
   }
   counter += dir;
 }
