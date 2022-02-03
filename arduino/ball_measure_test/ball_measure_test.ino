b7ce95342deab5dcc96b44a7f7c1b428e0c9f8b6
Jacopo Pellizzari <jacopisky@gmail.com>
2022-02-03 11:47:14 +0100

    Trial

--- arduino/ball_measure_test/ball_measure_test.ino
+++ arduino/ball_measure_test/ball_measure_test.ino
@@ -1,3 +1,5 @@
+#include<Math.h>
+
 // ULTRASONIC SENSOR
 // arduino pinout
 #define TRIGGER_PIN 3
@@ -5,12 +7,14 @@
 // setup
 #define CALIBRATION_LEN 10
 // constants
-#define SOUND_SPEED 343.0 // m/s
-
-// generic constants
+#define SOUND_SPEED 343.1 // m/s
+#define CM_TO_M 0.01
 #define US_TO_S 0.000001
 
-#define T_CONTROL 20000 //us
+// generic constants
+
+#define T_SENSE 1000000 //us
+#define T_CONTROL 2000000 //us
 
 // low-pass filter for motion measurements
 #define FILTER_b0 0.611015470351657
@@ -18,6 +22,8 @@
 #define FILTER_a1 -0.222030940703315
 
 #define CALIBRATION_LEN 10
+
+#define TH 0.3
 
 // ultrasonic sensor variables
 unsigned long start_t_meas, stop_t_meas;
@@ -37,6 +43,8 @@ float prev_ball_vel = 0;
 float prev_raw_ball_pos = 0;
 float prev_raw_ball_vel = 0;
 unsigned long t_actual_meas, t_prev_meas;
+
+unsigned long prev_control = 0;
 
 void change() {
   if (digitalRead(ECHO_PIN)) {
@@ -68,7 +76,7 @@ void setup() {
 }
 
 void loop() {
-  if (!measuring && micros() - prev_meas > T_CONTROL) {
+  if (!measuring && micros() - prev_meas > T_SENSE) {
     startMeasure();
   }
   if (is_meas_ready) {
@@ -76,8 +84,10 @@ void loop() {
     prev_meas = micros();
     t_actual_meas = prev_meas;
     measuring = false;
-    float round_time = (float)(stop_t_meas - start_t_meas);
-    measure = round_time / 2.0 * US_TO_S * SOUND_SPEED;
+    unsigned long round_time = stop_t_meas - start_t_meas;
+    measure = round_time / 2 * SOUND_SPEED * US_TO_S;
+    int cm = (int) (measure*100);
+    measure = cm * CM_TO_M;
     if (!calibration_finished) {
       offset += measure;
       j++;
@@ -92,16 +102,20 @@ void loop() {
     }
     else {
       updateBallDynamics(offset - measure);
-      Serial.print(ball_pos);
-      Serial.print(',');
-      Serial.println(ball_vel);
+      
+      if (micros() - prev_control > T_CONTROL) {
+          prev_control = micros();
+          Serial.println(ball_pos,5);
+      }
     }
   }
 }
 
 void updateBallDynamics(float raw_measure) {
+  //if(raw_measure - prev_raw_ball_pos > TH){return;}
+  //if(raw_measure - prev_raw_ball_pos < -TH){return;}
   ball_pos = FILTER_a1 * prev_ball_pos + FILTER_b0 * raw_measure + FILTER_b1 * prev_raw_ball_pos;
-  float raw_measure_vel = (ball_pos - prev_ball_pos)/((t_actual_meas - t_prev_meas) * US_TO_S);
+  float raw_measure_vel = (ball_pos - prev_ball_pos);///((t_actual_meas - t_prev_meas) * US_TO_S);
   ball_vel = FILTER_a1 * prev_ball_vel + FILTER_b0 * raw_measure_vel + FILTER_b1 * prev_raw_ball_vel;
   prev_raw_ball_vel = raw_measure_vel;
   prev_ball_vel = ball_vel;
